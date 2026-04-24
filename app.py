f91a19d40f5c1e919eae4a8e7ef1ae6c4e746efe
SannLim11 <limzhiying08@gmail.com>
2026-04-25 03:55:42 +0800

    add API key

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-client = genai.Client()
+client = genai.Client(api_key="GEMINI_API_KEY")
 
 @app.route("/review", methods=["POST"])
 def review_code():
